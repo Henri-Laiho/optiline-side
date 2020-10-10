0f60a6fdf90b54f4d921d93e869a44bbf42bd655
Hain Zuppur <h.zuppur@gmail.com>
2020-10-10 20:47:58 +0300

    Refactor

--- OpenCV/ImageToBinSignal.py
+++ OpenCV/ImageToBinSignal.py
@@ -1,24 +1,7 @@
 import cv2
 import numpy as np
 import wave, struct
-
-MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
-                    'C':'-.-.', 'D':'-..', 'E':'.',
-                    'F':'..-.', 'G':'--.', 'H':'....',
-                    'I':'..', 'J':'.---', 'K':'-.-',
-                    'L':'.-..', 'M':'--', 'N':'-.',
-                    'O':'---', 'P':'.--.', 'Q':'--.-',
-                    'R':'.-.', 'S':'...', 'T':'-',
-                    'U':'..-', 'V':'...-', 'W':'.--',
-                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
-                    '1':'.----', '2':'..---', '3':'...--',
-                    '4':'....-', '5':'.....', '6':'-....',
-                    '7':'--...', '8':'---..', '9':'----.',
-                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
-                    '?':'..--..', '/':'-..-.', '-':'-....-',
-                    '(':'-.--.', ')':'-.--.-'}
-
-decrypt = {v: k for k, v in MORSE_CODE_DICT.items()}
+from OpenCV.MorseCode import MorseDecoder
 
 cap = cv2.VideoCapture(0)
 height, width, channels = cap.read()[1].shape
@@ -28,18 +11,7 @@ x = int(width / 2 - box_size / 2)
 y = int(height / 2 - box_size / 2)
 w = h = box_size
 
-lastFrameWasOn = False
-lastFrameWasOff = False
-onTime = 0
-offTime = 0
-
-
-long = 10
-short = 5
-blank = 10
-
-currentWord = ""
-message = []
+decoder = MorseDecoder()
 
 while (True):
     gray = cv2.medianBlur(cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2GRAY), 5)
@@ -53,47 +25,13 @@ while (True):
 
     mean = cv2.mean(image, mask=mask)[:3]
 
-    if mean[0] >= 255 / 2:
-        #print(1)
-        lastFrameWasOn = True
-        onTime += 1
-
-        if lastFrameWasOff:
-            if offTime >= blank:
-                print()
-            lastFrameWasOff = False
-        offTime = 0
-
-    else:
-        #print(0)
-        lastFrameWasOff = True
-        offTime += 1
-
-        if lastFrameWasOn:
-            if onTime >= long:
-                print("-", end="")
-                currentWord += "-"
-            elif onTime >= short:
-                print(".", end="")
-                currentWord += "."
-            lastFrameWasOn = False
-            onTime = 0
-
-        if offTime >= blank:
-            if currentWord != "":
-                message.append(currentWord)
-                currentWord = ""
+    decoder.Send(mean[0] >= 255 / 2)
 
     # Displaying the image
     cv2.imshow("img", image)
 
     if cv2.waitKey(1) == 27:  # esc Key
-        #print(f"\n{message}")
-        text_message = ""
-
-        for i in message:
-            text_message += decrypt[i]
-        print(f"\n{text_message}")
+        print(f"\n{decoder.Get_Message()}")
         break
 
 cap.release()
---
+++ OpenCV/MorseCode.py
@@ -0,0 +1,68 @@
+class MorseDecoder:
+    MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                       'C': '-.-.', 'D': '-..', 'E': '.',
+                       'F': '..-.', 'G': '--.', 'H': '....',
+                       'I': '..', 'J': '.---', 'K': '-.-',
+                       'L': '.-..', 'M': '--', 'N': '-.',
+                       'O': '---', 'P': '.--.', 'Q': '--.-',
+                       'R': '.-.', 'S': '...', 'T': '-',
+                       'U': '..-', 'V': '...-', 'W': '.--',
+                       'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                       '1': '.----', '2': '..---', '3': '...--',
+                       '4': '....-', '5': '.....', '6': '-....',
+                       '7': '--...', '8': '---..', '9': '----.',
+                       '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                       '?': '..--..', '/': '-..-.', '-': '-....-',
+                       '(': '-.--.', ')': '-.--.-'}
+
+    decrypt = {v: k for k, v in MORSE_CODE_DICT.items()}
+
+    lastFrameWasOn = False
+    lastFrameWasOff = False
+    onTime = 0
+    offTime = 0
+
+    long = 10
+    short = 5
+    blank = 10
+
+    currentWord = ""
+    message = []
+
+    def Send(self, value):
+        if value:
+            self.lastFrameWasOn = True
+            self.onTime += 1
+
+            if self.lastFrameWasOff:
+                if self.offTime >= self.blank:
+                    print()
+                self.lastFrameWasOff = False
+            self.offTime = 0
+
+        else:
+            self.lastFrameWasOff = True
+            self.offTime += 1
+
+            if self.lastFrameWasOn:
+                if self.onTime >= self.long:
+                    print("-", end="")
+                    self.currentWord += "-"
+                elif self.onTime >= self.short:
+                    print(".", end="")
+                    self.currentWord += "."
+                self.lastFrameWasOn = False
+                self.onTime = 0
+
+            if self.offTime >= self.blank:
+                if self.currentWord != "":
+                    self.message.append(self.currentWord)
+                    self.currentWord = ""
+
+    def Get_Message(self):
+        text_message = ""
+
+        for i in MorseDecoder.message:
+            text_message += self.decrypt[i]
+
+        return text_message
